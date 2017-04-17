738ec7e29ac1ab7405b8854080842be602f63565
justin <justinledford@gmail.com>
2017-04-16 21:54:06 -0500

    remove fish

--- fish/.config/fish/aliases.fish
+++
@@ -1,7 +0,0 @@
-function ls
-    command ls --color=auto -N $argv
-end
-
-function irc
-    command ssh -t capybara 'tmux a'
-end
--- fish/.config/fish/config.fish
+++
@@ -1,7 +0,0 @@
-set NPM_PACKAGES ~/.npm-packages
-set PATH $PATH ~/bin ~/.gem/ruby/2.2.0/bin ~/.local/bin $NPM_PACKAGES/bin
-
-source ~/.config/fish/aliases.fish
-
-set fish_pager_color_prefix blue
-set fish_pager_color_description brblack
