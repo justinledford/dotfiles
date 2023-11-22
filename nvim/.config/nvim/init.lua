18fd0948e3bf6dbc0c413813a456f05f723b771e
Justin Ledford <justinledford@google.com>
2023-11-21 18:33:31 -0800

    replace YCM/ale/syntastic with various LSP stuff

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -46,22 +46,20 @@ vim.g.netrw_banner = 0
 -- Set leader key to space
 vim.g.mapleader = " "
 
-
+-------------------
+------ mappings
+-------------------
 
 -- Switch splits
-vim.api.nvim_set_keymap(
+vim.keymap.set(
   "n",
   "<C-X>",
   "<C-W><C-X>",
   { noremap = true }
 )
 
--------------------
------- mappings
--------------------
-
 -- Open Netrw listing
-vim.api.nvim_set_keymap(
+vim.keymap.set(
   "n",
   "<leader>e",
   ":Explore<cr>",
@@ -69,7 +67,7 @@ vim.api.nvim_set_keymap(
 )
 
 -- Delete current buffer
-vim.api.nvim_set_keymap(
+vim.keymap.set(
   "n",
   "<Leader>bd",
   ":bp\\|bd<Space>#<cr>",
@@ -77,7 +75,7 @@ vim.api.nvim_set_keymap(
 )
 
 -- Change all tabs to spaces
-vim.api.nvim_set_keymap(
+vim.keymap.set(
   "n",
   "<Leader>rt",
   ":retab<cr>",
@@ -85,21 +83,21 @@ vim.api.nvim_set_keymap(
 )
 
 -- Hit enter to clear search highlighting
-vim.api.nvim_set_keymap(
-  "n",
-  "<CR>",
-  ":noh<CR><CR>",
-  { noremap = true }
-)
+--vim.keymap.set(
+--  "n",
+--  "<CR>",
+--  ":noh<CR><CR>",
+--  { noremap = true }
+--)
 
 -- Switch to open buffers
-vim.api.nvim_set_keymap(
+vim.keymap.set(
   "n",
   "K",
   ":bnext<CR>",
   { noremap = true }
 )
-vim.api.nvim_set_keymap(
+vim.keymap.set(
   "n",
   "J",
   ":bprevious<CR>",
@@ -107,7 +105,7 @@ vim.api.nvim_set_keymap(
 )
 
 -- Reload all buffers
-vim.api.nvim_set_keymap(
+vim.keymap.set(
   "n",
   "<Leader>r",
   ":checkt<cr>",
@@ -115,27 +113,27 @@ vim.api.nvim_set_keymap(
 )
 
 -- ycm
-vim.api.nvim_set_keymap(
+vim.keymap.set(
   "n",
   "<Leader>gt",
   ":YcmCompleter GoTo<CR>",
   { noremap = true }
 )
-vim.api.nvim_set_keymap(
+vim.keymap.set(
   "n",
-  "<Leader>f",
-  ":YcmCompleter FixIt<CR>",
+  "<Leader>ca",
+  vim.lsp.buf.code_action,
   { noremap = true }
 )
 
 -- tagbar
-vim.api.nvim_set_keymap(
+vim.keymap.set(
   "n",
   "<Leader>t",
   ":TagbarToggle<CR>",
   { noremap = true }
 )
-vim.api.nvim_set_keymap(
+vim.keymap.set(
   "n",
   "<Leader>tj",
   ":TagbarOpen j<CR>",
@@ -143,19 +141,19 @@ vim.api.nvim_set_keymap(
 )
 
 -- fzf
-vim.api.nvim_set_keymap(
+vim.keymap.set(
   "n",
   "<Leader>o",
   ":Files<CR>",
   { noremap = true }
 )
-vim.api.nvim_set_keymap(
+vim.keymap.set(
   "n",
   "<Leader>b",
   ":Buffers<CR>",
   { noremap = true }
 )
-vim.api.nvim_set_keymap(
+vim.keymap.set(
   "n",
   "<Leader>a",
   ":Ag<CR>",
@@ -169,13 +167,9 @@ vim.api.nvim_set_keymap(
 vim.cmd([[
 call plug#begin()
 
-Plug 'ycm-core/YouCompleteMe', {'do': './install.py --clangd-completer'}
-
 Plug 'christoomey/vim-tmux-navigator'
 
 Plug 'ap/vim-buftabline'
-
-Plug 'vim-syntastic/syntastic'
 
 Plug 'airblade/vim-gitgutter'
 
@@ -186,14 +180,20 @@ Plug 'tpope/vim-abolish'
 
 Plug 'tpope/vim-fugitive'
 
-Plug 'ludovicchabant/vim-gutentags'
-
-Plug 'preservim/tagbar'
-
-Plug 'dense-analysis/ale'
-
 Plug 'mcchrish/zenbones.nvim', {'tag': 'v2.0.0'}
 Plug 'rktjmp/lush.nvim'
+
+Plug 'neovim/nvim-lspconfig', {'tag': 'v0.1.6'}
+Plug 'hrsh7th/nvim-cmp'
+Plug 'hrsh7th/cmp-nvim-lsp'
+Plug 'L3MON4D3/LuaSnip'
+Plug 'saadparwaiz1/cmp_luasnip'
+Plug 'rafamadriz/friendly-snippets'
+Plug 'creativenull/efmls-configs-nvim', { 'tag': 'v1.*' }
+Plug 'williamboman/mason.nvim'
+Plug 'williamboman/mason-lspconfig.nvim'
+Plug 'jose-elias-alvarez/null-ls.nvim'
+Plug 'jay-babu/mason-null-ls.nvim'
 
 call plug#end()
 ]])
@@ -203,28 +203,105 @@ call plug#end()
 -------------------
 
 vim.cmd([[
-autocmd CompleteDone * pclose
-let g:ycm_goto_buffer_command = 'split-or-existing-window'
-let g:ycm_global_ycm_extra_conf = ''
-
-let g:ale_linters = {
-\   'python': ['pylint'],
-\   'cpp': ['clangtidy'],
-\}
-let g:ale_virtualtext_cursor = 0
-
-let g:ale_fixers = {
-\   '*': ['remove_trailing_lines', 'trim_whitespace'],
-\   'python': ['black', 'isort'],
-\   'cpp': ['clang-format'],
-\}
-let g:ale_fix_on_save = 1
-
-let g:gutentags_cache_dir = "~/.cache/gutentags"
-]])
-
-vim.cmd([[
 set termguicolors
 set background=light
 colorscheme zenwritten
 ]])
+
+-- Use mason to install various LSPs, formatters and linters
+require('mason').setup()
+require('mason-lspconfig').setup({
+  ensure_installed = {
+    'clangd',
+    'efm',
+    'pyright',
+  }
+})
+require("mason-null-ls").setup({
+    ensure_installed = {
+      'black',
+      'isort',
+      'mypy',
+    }
+})
+
+-- Use nvm-cmp as a completion engine
+local cmp = require'cmp'
+local luasnip = require 'luasnip'
+require('luasnip.loaders.from_vscode').lazy_load()
+luasnip.config.setup {}
+
+cmp.setup {
+  snippet = {
+    expand = function(args)
+      luasnip.lsp_expand(args.body)
+    end,
+  },
+  completion = {
+    completeopt = 'menu,menuone,noinsert'
+  },
+  mapping = cmp.mapping.preset.insert {
+    ['<C-n>'] = cmp.mapping.select_next_item(),
+    ['<C-p>'] = cmp.mapping.select_prev_item(),
+    ['<C-d>'] = cmp.mapping.scroll_docs(-4),
+    ['<C-f>'] = cmp.mapping.scroll_docs(4),
+    ['<C-Space>'] = cmp.mapping.complete {},
+    ['<CR>'] = cmp.mapping.confirm {
+      behavior = cmp.ConfirmBehavior.Replace,
+      select = true,
+    },
+    ['<Tab>'] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_next_item()
+      elseif luasnip.expand_or_locally_jumpable() then
+        luasnip.expand_or_jump()
+      else
+        fallback()
+      end
+    end, { 'i', 's' }),
+    ['<S-Tab>'] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_prev_item()
+      elseif luasnip.locally_jumpable(-1) then
+        luasnip.jump(-1)
+      else
+        fallback()
+      end
+    end, { 'i', 's' }),
+  },
+  sources = {
+    { name = 'nvim_lsp' },
+    { name = 'luasnip' },
+  },
+}
+
+-- Configure each LSP
+local capabilities = require('cmp_nvim_lsp').default_capabilities()
+require'lspconfig'.clangd.setup {
+  capabilities = capabilities,
+}
+require'lspconfig'.pyright.setup {
+  capabilities = capabilities,
+}
+require "lspconfig".efm.setup {
+  init_options = {documentFormatting = true},
+  capabilities = capabilities,
+  settings = {
+    rootMarkers = {".git/"},
+    languages = {
+      python = {
+        require('efmls-configs.formatters.black'),
+        require('efmls-configs.formatters.isort'),
+        -- TODO: add no-untyped-def
+        require('efmls-configs.linters.mypy'),
+      }
+    }
+  }
+}
+
+-- Format buffer on write
+vim.api.nvim_create_autocmd("BufWritePre", {
+  callback = function()
+    vim.lsp.buf.formatting()
+  end,
+})
