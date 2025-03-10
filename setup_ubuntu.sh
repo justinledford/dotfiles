968326843733c2ae86a84281f86a2b32ef3b2bb8
Justin Ledford <justinledford@gmail.com>
2025-03-09 19:28:28 -0700

    clean up

--- setup_ubuntu.sh
+++ setup_ubuntu.sh
@@ -2,30 +2,9 @@
 
 set -xe
 
-# install stuff
-sudo apt -y install \
-  neovim curl git tmux build-essential cmake python3-dev stow universal-ctags \
-  python3-venv python3-virtualenvwrapper \
-  google-chrome-stable
-
-# setup bash
-
-# remove user bashrc if its the same as the system bashrc so we can just
-# source the system bashrc in the stowed bashrc
-diff ~/.bashrc /etc/skel/.bashrc && rm ~/.bashrc
-stow bash
-
-# setup nvim
-stow nvim
-
-## install packer
-git clone --depth 1 https://github.com/wbthomason/packer.nvim \
-  ~/.local/share/nvim/site/pack/packer/start/packer.nvim
-
-## install plugins
-nvim -c ":PackerInstall" -c ":qa"
-
 # setup tmux
+sudo apt -y install tmux
+
 stow tmux
 TPM_DIR=~/.tmux/plugins/tpm
 [ ! -d "${TPM_DIR}" ] && git clone https://github.com/tmux-plugins/tpm ${TPM_DIR}
