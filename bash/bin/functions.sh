07317a339149047926716afb6e5261a42383ec58
justin ledford <justinledford@gmail.com>
2026-01-10 18:05:12 -0800

    function.sh: simplify dtach commands

--- bash/bin/functions.sh
+++ bash/bin/functions.sh
@@ -1,6 +1,6 @@
-function dtach_new() {
+function dt() {
   if [[ $# -ne 1 ]]; then
-    echo >&2 "usage: dtach-new name"
+    echo >&2 "usage: dt NAME"
     return 1
   fi
 
@@ -8,24 +8,10 @@ function dtach_new() {
   local -r sessions_name="${1}"
 
   mkdir -p "${sessions_dir}"
-  dtach -c "${sessions_dir}/${sessions_name}" bash
+  dtach -A "${sessions_dir}/${sessions_name}" bash
 }
 
-function dtach_attach() {
-  if [[ $# -ne 1 ]]; then
-    echo >&2 "usage: dtach-attach name"
-    return 1
-  fi
-
+function dtl() {
   local -r sessions_dir="${HOME}/.dtach_sessions"
-  local -r sessions_name="${1}"
-
-  mkdir -p "${sessions_dir}"
-  dtach -a "${sessions_dir}/${sessions_name}"
-}
-
-function dtach_list() {
-  local -r sessions_dir="${HOME}/.dtach_sessions"
-
   ls "${sessions_dir}"
 }
